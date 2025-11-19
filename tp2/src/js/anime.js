63fbe20d5940c3486b63548fd6fc7b464910027a
mariam <mariamgevorgyan@hotmail.com>
2025-11-18 22:36:45 -0500

    animejs

--- tp2/src/js/anime.js
+++ tp2/src/js/anime.js
@@ -1,5 +1,63 @@
 import { animate } from 'animejs';
 
-animate('.dot', {
-  // ...
+animate(".hautgauche img", {
+    keyframes: [
+        { rotate: -4, duration: 10000, easing: "easeInOutSine" },
+        { rotate: 0, duration: 10000, easing: "easeInOutSine" }
+    ],
+    direction: "alternate",
+    loop: true,
+    delay: 150
+});
+
+animate(".hautdroite img", {
+    keyframes: [
+        { rotate: 4, duration: 10000, easing: "easeInOutSine" },
+        { rotate: 0, duration: 10000, easing: "easeInOutSine" }
+    ],
+    direction: "alternate",
+    loop: true,
+    delay: 150
+});
+
+// Ailes du milieu (battement un peu plus discret)
+animate(".milieugauche img", {
+    keyframes: [
+        { rotate: -5, duration: 10000, easing: "easeInOutSine" },
+        { rotate: 0, duration: 10000, easing: "easeInOutSine" }
+    ],
+    direction: "alternate",
+    loop: true,
+    delay: 150
+});
+
+animate(".milieudroite img", {
+    keyframes: [
+        { rotate: 5, duration: 10000, easing: "easeInOutSine" },
+        { rotate: 0, duration: 10000, easing: "easeInOutSine" }
+    ],
+    direction: "alternate",
+    loop: true,
+    delay: 150
+});
+
+// Ailes du bas (plus léger)
+animate(".basgauche img", {
+    keyframes: [
+        { rotate: -4, duration: 10000, easing: "easeInOutSine" },
+        { rotate: 0, duration: 10000, easing: "easeInOutSine" }
+    ],
+    direction: "alternate",
+    loop: true,
+    delay: 300
+});
+
+animate(".basdroite img", {
+    keyframes: [
+        { rotate: 4, duration: 10000, easing: "easeInOutSine" },
+        { rotate: 0, duration: 10000, easing: "easeInOutSine" }
+    ],
+    direction: "alternate",
+    loop: true,
+    delay: 300
 });
