2d01dd644a1ea0740b46daf7bc7efd3bd736afdf
mariam <mariamgevorgyan@hotmail.com>
2025-12-01 17:32:12 -0500

    ok

--- tp2/src/js/vfx.js
+++ tp2/src/js/vfx.js
@@ -3,14 +3,13 @@ import { VFX } from "vfx-js";
 const vfx = new VFX();
 
 // Sélectionne UNIQUEMENT les images Rei
-document.querySelectorAll(".rei-wrapper").forEach((img) => {
+document.querySelectorAll(".image-title").forEach((img) => {
   vfx.add(img, {
-    shader: "glitch",
-    mode: "mesh",
+    shader: "rgbShift",
     distortion: 0.02,
     chroma: 0.01,
     shake: 0.01,
     speed: 0.15,
-    overflow: 5
+    overflow: 2
   });
 });
