a48abf6cc51aa95385ea55bd7466d55d18cc85ba
mariam <mariamgevorgyan@hotmail.com>
2025-11-30 19:56:16 -0500

    bug

--- tp2/src/js/anime.js
+++ tp2/src/js/anime.js
@@ -20,6 +20,7 @@ animate("#hautdroite", {
     delay: 150
 });
 
+// Ailes du milieu (battement un peu plus discret)
 animate("#milieugauche", {
     keyframes: [
         { rotate: -5, duration: 10000, easing: "easeInOutSine" },
@@ -40,6 +41,7 @@ animate("#milieudroite", {
     delay: 150
 });
 
+// Ailes du bas (plus léger)
 animate("#basgauche", {
     keyframes: [
         { rotate: -4, duration: 10000, easing: "easeInOutSine" },
@@ -61,11 +63,12 @@ animate("#basdroite", {
 }); 
 
 window.addEventListener("DOMContentLoaded", () => {
-    anime({
+  // Animation d'apparition des blocs UI
+  anime({
     targets: ".placeholder",
     opacity: [0, 1],
     translateY: [25, 0],
-    delay: anime.stagger(120, { start: 400 }),
+    delay: anime.stagger(120, { start: 400 }), // animation en cascade
     duration: 800,
     easing: "easeOutQuad"
   });
